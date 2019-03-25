5d7af336aa56ac7cdb99ee4c698b795657f1e8d8
Stefan <stscit21@hs-esslingen.de>
2019-03-25 15:55:29 +0100

    Use EventGridPanel now

--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -1,20 +1,24 @@
 package main;
 
+import control.Controller;
 import data.grid.Grid2D;
 import data.grid.impl.Grid2DImpl;
-import ui.GridPanel;
+import ui.EventGridPanel;
 
 import javax.swing.*;
 
 public class Main {
 
     public static void main(String[] args) {
-        Grid2D<?> dataGrid = new Grid2DImpl<>(3);
+        Grid2D<Character> dataGrid = new Grid2DImpl<>(3);
+        EventGridPanel gridPanel = new EventGridPanel(dataGrid);
+
+        Controller controller = new Controller(dataGrid, gridPanel);
 
         JFrame frame = new JFrame();
+        frame.setSize(500, 500);
         frame.setTitle("Tic Tac Toe");
 
-        GridPanel gridPanel = new GridPanel(dataGrid);
 
         frame.setContentPane(gridPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
