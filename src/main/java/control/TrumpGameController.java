56769e863ac2f11ed660f14cb5e1d410d476d36d
Stefan <stscit21@hs-esslingen.de>
2019-04-04 23:41:00 +0200

    Add function to increase movement speed

--- src/main/java/control/TrumpGameController.java
+++ src/main/java/control/TrumpGameController.java
@@ -13,6 +13,7 @@ import java.util.Timer;
 import java.util.TimerTask;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.function.Function;
 
 public class TrumpGameController {
 
@@ -26,6 +27,8 @@ public class TrumpGameController {
 
     private Trump trump;
     private Coin coin;
+
+    private long ellapsedTime = 0;
 
     public TrumpGameController(ExecutorService executorService, Container container, GameData gameData) {
         this.executorService = executorService;
@@ -54,12 +57,14 @@ public class TrumpGameController {
         MovementController movementController = new KeyInputController(trumpMoveableObject, KeyEvent.VK_W, KeyEvent.VK_D, KeyEvent.VK_S, KeyEvent.VK_A);
         MovementController controller = new MouseMotionController(trumpMoveableObject);
         MovementController mouseController = new MouseClickController(trumpMoveableObject);
-        trumpMoveableObject.setMovementController(controller);
-        trumpMoveableObject.setMovementSpeed(45.0);
+        trumpMoveableObject.setMovementController(movementController);
+        trumpMoveableObject.setMovementSpeed(5.0);
         trumpMoveableObject.getMovementController().register(this.container);
 
         container.add(trumpComponent);
         container.add(coinGameComponent);
+        container.setFocusable(true);
+        container.requestFocus();
 
         TimerTask task = new TimerTask() {
             @Override
@@ -86,6 +91,11 @@ public class TrumpGameController {
 
     private void gameTick(long delta, TimeUnit timeUnit) {
         trump.gameTick(delta, timeUnit);
+        ellapsedTime += timeUnit.convert(delta, TimeUnit.NANOSECONDS);
+        MoveableObject moveableObject = trump.getMoveableObject();
+        Function<Long, Double> movementFunction = x -> (((1 / (double) 400) * Math.pow(x * Math.pow(10, -9), 2)) + 20.0);
+        Double movementSpeed = movementFunction.apply(ellapsedTime);
+        moveableObject.setMovementSpeed(movementSpeed);
     }
 
     public void shutdown() {
