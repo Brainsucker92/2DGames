e8431eb561f41d08545f2a63e9dec5ca40632db3
Stefan <stscit21@hs-esslingen.de>
2019-04-06 03:16:00 +0200

    Add american flag resource

--- src/main/java/data/Resources.java
+++ src/main/java/data/Resources.java
@@ -6,6 +6,8 @@ import java.nio.file.Paths;
 
 public class Resources {
 
+    public static final ImageResource AMERICAN_FLAG = new ImageResource(Paths.get("img/american_flag.png"));
+
     public static final ImageResource BIRD = new ImageResource(Paths.get("ui/sprites/flappy.png"));
     public static final ImageResource TRUMP = new ImageResource(Paths.get("ui/sprites/trump_run.png"));
     public static final ImageResource COIN = new ImageResource(Paths.get("ui/sprites/coin.png"));
